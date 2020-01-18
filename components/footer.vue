9e95c7765836a7e3f5ec324133c6c0af5416f3d1
JC-Chan <1446822779@qq.com>
2020-01-18 16:37:26 +0800

    页脚完成

--- components/footer.vue
+++ components/footer.vue
@@ -1,10 +1,38 @@
 <template>
-  <div>页脚</div>
+  <div class="footer-wrapper">
+    <el-row class="info-list">
+      <el-col :span="12" class="left">
+        <p>地址: 涧西区蓬莱路2号B区毕昇园10号楼</p>
+      </el-col>
+      <el-col :span="12" class="right">
+        <p>技术支持: 河南品讯网络科技有限公司</p>
+        <p>备案: 豫ICP备125462586-1号</p>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="less" scoped>
+.footer-wrapper {
+  height: 90px;
+//   padding: 10px 0 20px 0;
+  padding: 15px;
+  background-color: #323232;
+  color: #fff;
+  .info-list {
+    width: 1200px;
+    margin: auto;
+    font-size: 14px;
+
+    .right {
+        p{
+            margin-bottom: 10px;
+        }
+    }
+  }
+}
 </style>
